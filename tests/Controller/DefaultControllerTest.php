7d3a69cb9dde7c6701f8d8193754f8dd295fcb7c
Takashi Kanemoto <ttskch@gmail.com>
2020-03-15 01:36:51 +0900

    test: 💍 yamlフィクスチャを使って機能テストを修正

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -3,16 +3,27 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Liip\TestFixturesBundle\Test\FixturesTrait;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UserControllerTest extends WebTestCase
 {
-    public function testShow()
+    use FixturesTrait;
+
+    protected function setUp(): void
+    {
+        $this->loadFixtureFiles([
+            __DIR__ . '/../../tests/fixtures/users.yaml',
+        ]);
+    }
+
     public function testShow(): void
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/1/show');
-        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $this->assertEquals('Name: Takashi', $crawler->text());
     }
 }
